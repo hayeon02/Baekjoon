6d8e4174be5638d3225bed397423289c98a61e82
hayeon02 <85230479+hayeon02@users.noreply.github.com>
2024-05-09 02:49:11 +0900

    [Bronze III] Title: 주사위 게임, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2476. 주사위 게임/주사위 게임.py
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -1,23 +1,15 @@
 num = int(input())
-total = 0
-money = 0
+total = []
+
 for i in range(num):
     a, b, c = map(int, input().split())
     d = [a, b, c]
     if a == b and b == c:
-        total = 10000 + (a * 1000)
-        if money < total:
-            money = total
+        total.append(10000 + (a * 1000))
     elif a == b or a == c:
-        total = 1000 + (a * 100)
-        if money < total:
-            money = total
+        total.append(1000 + (a * 100))
     elif b == c or b == a:
-        total = 1000 + (b * 100)
-        if money < total:
-            money = total
+        total.append(1000 + (b * 100))
     else:
-        total = max(d) * 100
-        if money < total:
-            money = total
-print(money)
+        total.append(max(d) * 100)
+print(max(total))
